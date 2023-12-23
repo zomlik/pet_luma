becb39ee95f61807a204229ac13818c31fd06b15
Zomlik <sanek77594@yandex.ru>
2023-12-23 07:26:02 +0300

    fixed items test

--- tests/test_items.py
+++ tests/test_items.py
@@ -20,8 +20,3 @@ class TestMiniItems:
         page.choose_item_and_add_to_wish_list()
         with allure.step("ОР: Чтобы добавить товар в wishlist нужно быть авторизаванным"):
             assert page.get_message_login_page() == "You must login or register to add items to your wishlist."
-
-    def test_add_item_catr_with_options(self, browser):
-        page = MiniItem(browser)
-        page.open(URL.BASE_URL)
-        page.
