1dd891fbc6a52ef0318d6b88707aabdad58540d7
Александр Тихонов <69787064+zomlik@users.noreply.github.com>
2024-10-14 09:47:54 +0300

    Delete pages directory

--- pages/my_account.py
+++
@@ -1,7 +0,0 @@
-from base.base_page import BasePage
-from locators.my_account_locators import MyAccountLocators
-
-
-class MyAccount(BasePage):
-    def switch_button(self):
-        return self.is_clickable(MyAccountLocators.SWITCH_BUTTON).click()
